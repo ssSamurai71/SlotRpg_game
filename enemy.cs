b347c99f71f3f91591e27f88776aec05da9e439b
Arthur <66388988+ssSamurai71@users.noreply.github.com>
2021-11-16 21:13:40 -0800

    fixed script

--- enemy.cs
+++ enemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class e : MonoBehaviour
+public class enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- main_loop.cs
+++ main_loop.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class main_loop: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
