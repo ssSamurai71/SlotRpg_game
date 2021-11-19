1d5f9aa26440e91c563e42a1fac2706e53c6f700
Arthur <66388988+ssSamurai71@users.noreply.github.com>
2021-11-18 22:42:47 -0800

    base slot set up

--- slots.cs
+++ slots.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class slots : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private string[,,,,] slot = new array[5,5,5,5,5];
+    private string[] slot_options = new array[] {'PAtk', 'PDef', 'EAtk', 'HEAL', 'COIN', 'NONE'};
+    public Random rand = new Random();
+
+    public void create_slot()
     {
-        
+        for (int row = 0; row < slot.length; row++)
+        {
+            for (int column = 0; i < slot[row].length; i++)
+            {
+                slot[row,column] = slot_options[5];
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void spin_slot()
     {
-        
+        for (int row = 0; row < slot.length; row++)
+        {
+            for (int column = 0; i < slot[row].length; i++)
+            {
+                int random_num = rand.Next(0,5);
+                slot[row,column] = slot_options[5];
+            }
+        }
+    }
+
+    public void display_reel()
+    {
+        for (int row = 0; row < slot.length; row++)
+        {
+            public string display_line = "";
+            for (int column = 0; i < slot[row].length; i++)
+            {
+                string.Concat(display_line,slot[row,column];)
+                
+            }
+        }
     }
 }
